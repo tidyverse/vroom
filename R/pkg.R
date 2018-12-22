767f3ba1a261b772ea1233647bb1ae8cc4f3fdcd
Jim Hester <james.f.hester@gmail.com>
2018-12-21 22:13:28 -0500

    Return a tibble

--- R/pkg.R
+++ R/pkg.R
@@ -1,3 +1,11 @@
 #' @useDynLib readidx, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 NULL
+
+#' @export
+read_tsv <- function(x) {
+  out <- create_index_(x)
+  names(out) <- letters[seq_len(length(out))]
+
+  tibble::as_tibble(out)
+}
