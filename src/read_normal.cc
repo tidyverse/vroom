a4354da356fac3b684c2573b59e710e90f53eefc
Jim Hester <james.f.hester@gmail.com>
2019-01-03 16:52:22 -0500

    Simple sequential version of parsing factors
    - Does not currently handle missing values

--- src/read_normal.cc
+++ src/read_normal.cc
@@ -38,3 +38,52 @@ Rcpp::LogicalVector read_lgl(
 
   return out;
 }
+
+Rcpp::IntegerVector read_fctr(
+    std::shared_ptr<std::vector<size_t> > offsets,
+    mio::shared_mmap_source mmap,
+    R_xlen_t column,
+    R_xlen_t num_columns,
+    R_xlen_t skip,
+    R_xlen_t num_threads) {
+
+  R_xlen_t n = offsets->size() / num_columns - skip;
+
+  Rcpp::IntegerVector out(n);
+  std::vector<std::string> levels;
+  std::map<std::string, int> level_map;
+
+  auto p = out.begin();
+
+  int max_level = 1;
+
+  parallel_for(
+      n,
+      [&](int start, int end, int id) {
+        size_t idx = (start + skip) * num_columns + column;
+        for (int i = start; i < end; ++i) {
+          size_t cur_loc = (*offsets)[idx];
+          size_t next_loc = (*offsets)[idx + 1] - 1;
+          size_t len = next_loc - cur_loc;
+
+          auto str = std::string(mmap.data() + cur_loc, len);
+
+          auto val = level_map.find(str);
+          if (val != level_map.end()) {
+            p[i] = val->second;
+          } else {
+            p[i] = max_level;
+            level_map[str] = max_level++;
+            levels.emplace_back(str);
+          }
+          idx += num_columns;
+        }
+      },
+      num_threads,
+      false);
+
+  out.attr("levels") = levels;
+  out.attr("class") = "factor";
+
+  return out;
+}
--- src/read_normal.h
+++ src/read_normal.h
@@ -11,3 +11,11 @@ Rcpp::LogicalVector read_lgl(
     R_xlen_t num_columns,
     R_xlen_t skip,
     R_xlen_t num_threads);
+
+Rcpp::IntegerVector read_fctr(
+    std::shared_ptr<std::vector<size_t> > offsets,
+    mio::shared_mmap_source mmap,
+    R_xlen_t column,
+    R_xlen_t num_columns,
+    R_xlen_t skip,
+    R_xlen_t num_threads);
