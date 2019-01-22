cca4a30babf417b97d17fc71539892c6f28d777a
Jim Hester <james.f.hester@gmail.com>
2019-01-22 16:18:01 -0500

    Move start and end to next full line

--- src/index.cc
+++ src/index.cc
@@ -22,6 +22,16 @@ using namespace vroom;
 // return start;
 //}
 
+size_t find_next_newline(const mio::mmap_source& mmap_, size_t start) {
+  auto begin = mmap_.data() + start;
+  auto res =
+      static_cast<const char*>(memchr(begin, '\n', mmap_.size() - start));
+  if (!res) {
+    return start;
+  }
+  return res - mmap_.data();
+}
+
 index::index(
     const char* filename,
     const char delim,
@@ -56,6 +66,10 @@ index::index(
         values[id].reserve(128);
         if (id == 0) {
           values[id].push_back(0);
+          end = find_next_newline(mmap_, end);
+        } else {
+          start = find_next_newline(mmap_, start);
+          end = find_next_newline(mmap_, end);
         }
         // Rcpp::Rcerr << "Indexing start: ", v.size() << '\n';
         index_region(mmap_, values[id], delim, start, end, id);
