4462ad73622d34c96294d3d0ceb6de8445ee70c1
Jim Hester <james.f.hester@gmail.com>
2019-02-19 14:40:14 -0500

    Re-export col functions from readr

---
+++ R/readr.R
@@ -0,0 +1,39 @@
+#' @importFrom readr col_integer
+#' @export
+readr::col_integer
+
+#' @importFrom readr col_factor
+#' @export
+readr::col_factor
+
+#' @importFrom readr col_double
+#' @export
+readr::col_double
+
+#' @importFrom readr col_character
+#' @export
+readr::col_character
+
+#' @importFrom readr col_factor
+#' @export
+readr::col_factor
+
+#' @importFrom readr col_skip
+#' @export
+readr::col_skip
+
+#' @importFrom readr col_guess
+#' @export
+readr::col_guess
+
+#' @importFrom readr cols
+#' @export
+readr::cols
+
+#' @importFrom readr cols_only
+#' @export
+readr::cols_only
+
+#' @importFrom readr cols_condense
+#' @export
+readr::cols_condense
