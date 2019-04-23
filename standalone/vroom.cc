88dbdfcc9557e92899d2c7ede8fe732f6241843c
Jim Hester <james.f.hester@gmail.com>
2019-04-23 15:10:57 -0400

    Use the escape and comment options when fuzzing
    To hopefully catch some more bugs

--- standalone/vroom.cc
+++ standalone/vroom.cc
@@ -12,9 +12,17 @@ int main(int argc, char** argv) {
   app.add_option("-d,--delim", delim, "The delimiter to use (default: tab)");
   int num_threads = 8;
   app.add_option("-t,--num_threads", num_threads, "The number of parallel threads to use");
+  bool escape_double = false;
+  app.add_flag("--escape_double", escape_double, "Quotes escaped by doubling them");
+  bool escape_backslash = false;
+  app.add_flag("--escape_backslash", escape_backslash, "Quotes escaped by backslash escaping them");
+  std::string comment;
+  app.add_option("--comment", comment, "Character to use as a comment");
+  size_t skip = 0;
+  app.add_option("-s,--skip", skip, "Number of lines to skip");
   CLI11_PARSE(app, argc, argv);
 
-  auto idx = vroom::delimited_index(filename.c_str(), delim.c_str(), '"', false, false, false, false, 0, -1, '\0', num_threads, false, false);
+  auto idx = vroom::delimited_index(filename.c_str(), delim.c_str(), '"', false, escape_double, escape_backslash, false, skip, -1, comment.c_str()[0], num_threads, false, false);
 
   std::cout << idx.num_rows() * idx.num_columns() << '\n';
 
