0ad926ffdef0c2942104fdf0b643eafde96993b8
Jim Hester <james.f.hester@gmail.com>
2019-04-26 11:46:38 -0400

    Add vroom_examples() function
    This ensures vroom_example is type stable

--- R/example.R
+++ R/example.R
@@ -1,19 +1,26 @@
 #' Get path to vroom examples
 #'
 #' vroom comes bundled with a number of sample files in
-#' its 'inst/extdata' directory. This function make them
-#' easy to access
-#' @param path Name of file. If 'NULL', all available example files will be
+#' its 'inst/extdata' directory. Use `vroom_examples()` to list all the
+#' available examples and `vroom_example()` to retrieve the path to one
+#' example.
+#' @param path Name of file.
+#' @param pattern A regular expression of filenames to match. If `NULL` all
+#'   available files are returned.
 #' listed.
 #' @export
 #' @examples
-#' vroom_example()
+#' # List all available examples
+#' vroom_examples()
+#'
+#' # Get path to one example
 #' vroom_example("mtcars.csv")
-vroom_example <- function (path = NULL) {
-  if (is.null(path)) {
-    dir(system.file("extdata", package = "vroom"))
-  }
-  else {
-    system.file("extdata", path, package = "vroom", mustWork = TRUE)
-  }
+vroom_example <- function (path) {
+  system.file("extdata", path, package = "vroom", mustWork = TRUE)
+}
+
+#' @rdname vroom_example
+#' @export
+vroom_examples <- function (pattern = NULL) {
+  list.files(system.file("extdata", package = "vroom"), pattern = pattern)
 }
