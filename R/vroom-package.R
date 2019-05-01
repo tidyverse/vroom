7bd66aa34c287370cc8fdee35c3a11231f8473e2
Jim Hester <james.f.hester@gmail.com>
2019-05-01 09:03:41 -0400

    Add package level documentation

---
+++ R/vroom-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @aliases vroom-package
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
