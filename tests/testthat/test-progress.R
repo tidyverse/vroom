4ca9679409504825c260a2a3f91a608dfa39ab9e
Jim Hester <james.f.hester@gmail.com>
2019-05-10 09:06:47 -0400

    Delay start of threads, so we can capture the output

--- tests/testthat/test-progress.R
+++ tests/testthat/test-progress.R
@@ -2,7 +2,7 @@ test_that("The progress bar works", {
   f <- tempfile()
   on.exit(unlink(f))
 
-  withr::with_envvar(c("VROOM_PROGRESS_SHOW_AFTER" = "0", "VROOM_PROGRESS_UPDATE_INTERVAL" = "0"), {
+  withr::with_envvar(c("VROOM_PROGRESS_SHOW_AFTER" = "0", "VROOM_PROGRESS_UPDATE_INTERVAL" = "0", "VROOM_PROGRESS_THREAD_SLEEP" = ".25"), {
     withr::with_message_sink(f,
       vroom("abc\n123\n", col_types = list(), progress = TRUE)
     )
