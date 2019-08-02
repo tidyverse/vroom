a6caf6971fe14828ce44a11fb7ecd70a89765d86
Jim Hester <james.f.hester@gmail.com>
2019-08-02 13:05:24 -0400

    Use the multi reporter to output junit results

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
 library(vroom)
 
-test_check("vroom")
+test_check("vroom", reporter = MultiReporter$new(reporters = list(CheckReporter$new(), JunitReporter$new(file = "test-results.xml"))))
