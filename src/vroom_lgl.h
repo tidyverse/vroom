4fedaecfe59fafbe3a266e40f9c4353517b2a7c0
Jim Hester <james.f.hester@gmail.com>
2020-06-30 14:26:17 -0400

    Convert vroom_lgl

--- src/vroom_lgl.h
+++ src/vroom_lgl.h
@@ -1,9 +1,9 @@
 #pragma once
 
+#include <cpp11/logicals.hpp>
+
 #include "parallel.h"
 #include "vroom_vec.h"
-
-#include <Rcpp.h>
 
 const static char* const true_values[] = {
     "T", "t", "True", "TRUE", "true", (char*)NULL};
@@ -33,29 +33,29 @@ inline bool isFalse(const char* start, const char* end) {
   return false;
 }
 
-inline int
+inline Rboolean
 parse_logical(const char* start, const char* end, bool strict = true) {
   auto len = end - start;
 
   if (isTrue(start, end) || (!strict && len == 1 && *start == '1')) {
-    return true;
+    return TRUE;
   }
   if (isFalse(start, end) || (!strict && len == 1 && *start == '0')) {
-    return false;
+    return FALSE;
   }
   return NA_LOGICAL;
 }
 
-inline Rcpp::LogicalVector read_lgl(vroom_vec_info* info) {
+inline cpp11::logicals read_lgl(vroom_vec_info* info) {
 
   R_xlen_t n = info->column->size();
 
-  Rcpp::LogicalVector out(n);
+  cpp11::writable::logicals out(n);
 
   parallel_for(
       n,
       [&](size_t start, size_t end, size_t id) {
-        auto i = start;
+        R_xlen_t i = start;
         auto col = info->column->slice(start, end);
         for (const auto& str : *col) {
           out[i++] = parse_logical(str.begin(), str.end(), false);
