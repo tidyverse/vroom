81afde37c968904c8e5c0caa633fedc7a00920f6
Jim Hester <james.f.hester@gmail.com>
2021-06-30 15:52:23 -0400

    Install archive sys deps on mac and linux

--- src/fixed_width_index_connection.cc
+++ src/fixed_width_index_connection.cc
@@ -10,6 +10,7 @@
 #include <future> // std::async, std::future
 #include <utility>
 
+
 #ifdef VROOM_LOG
 #include "spdlog/sinks/basic_file_sink.h" // support for basic file logging
 #include "spdlog/spdlog.h"
