0be749e53157b1dd8d43152bda22ff9de02d1d91
Jim Hester <james.f.hester@gmail.com>
2021-09-10 14:08:49 -0400

    Use const rather than a macro

--- src/vroom_big_int.h
+++ src/vroom_big_int.h
@@ -4,14 +4,12 @@
 
 #include "altrep.h"
 
-#define NA_INTEGER64 (0x8000000000000000)
+const long long NA_INTEGER64 = 0x8000000000000000LL;
 
 #include "vroom.h"
 
 namespace cpp11 {
-inline bool is_na(long long x) {
-  return static_cast<unsigned long>(x) == NA_INTEGER64;
-}
+inline bool is_na(long long x) { return x == NA_INTEGER64; }
 } // namespace cpp11
 
 namespace vroom {
