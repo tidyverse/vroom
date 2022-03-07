2d39fb7f7cd3ebf27dea1ad85731bcddd3e5b921
shelby <scbearrows@gmail.com>
2022-03-07 10:59:44 -0700

    `usethis::use_package_doc()`

--- R/vroom-package.R
+++ R/vroom-package.R
@@ -1,10 +1,6 @@
 #' @keywords internal
-#' @aliases vroom-package
 "_PACKAGE"
 
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @importFrom lifecycle deprecated deprecate_warn
 ## usethis namespace: end
 NULL
