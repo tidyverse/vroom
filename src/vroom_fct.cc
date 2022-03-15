dc40e17c5ded3431e13e2f2bde19c90dc031d330
shelby <scbearrows@gmail.com>
2022-03-15 13:42:54 -0600

    `level_map` should update

--- src/vroom_fct.cc
+++ src/vroom_fct.cc
@@ -73,8 +73,8 @@ cpp11::integers read_fct_implicit(vroom_vec_info* info, bool include_na) {
           if (na_level == NA_INTEGER) {
             na_level = max_level++;
             levels.push_back(NA_STRING);
-            level_map[str.str()] = na_level;
           }
+          level_map[str.str()] = na_level;
           out[i++] = na_level;
         } else {
           out[i++] = NA_INTEGER;
