1f31e9635f7545c7570252b6f15e8e601acb44af
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-05-08 16:27:41 -0700

    Add second example

--- debug/path-encoding-debug.R
+++ debug/path-encoding-debug.R
@@ -1,3 +1,7 @@
 tfile <- withr::local_file("b\u00e9.csv")
 writeLines(c("a,b", "A,B"), tfile)
 vroom::vroom(tfile, show_col_types = FALSE)
+
+tfile <- withr::local_file("b\u00e9.csv")
+writeChar("a,b\nA,B", con = tfile, eos = NULL)
+vroom::vroom(tfile, show_col_types = FALSE)
