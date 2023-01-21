9d6b5b99c1c9902a536c81785e78dfbcb996f14a
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-01-20 17:15:26 -0800

    Add a test

--- tests/testthat/test-col_types.R
+++ tests/testthat/test-col_types.R
@@ -22,3 +22,15 @@ test_that("col_types are truncated if you pass too many (#355)", {
   res <- vroom(I("a,b,c,d\n1,2,3,4"), col_types = "cccccccc")
   expect_equal(res, tibble::tibble(a = "1", b = "2", c = "3", d = "4"))
 })
+
+test_that("all col_types can be reported with color", {
+  local_reproducible_output(crayon = TRUE)
+  dat <- vroom(
+    I(glue::glue("
+      skip,guess,character,factor,logical,double,integer,big_integer,\\
+      number,date,datetime
+      skip,a,b,c,TRUE,1.3,5,10,\"1,234.56\",2023-01-20,2018-01-01 10:01:01")),
+    col_types = "_?cfldiInDT"
+  )
+  expect_snapshot(spec(dat))
+})
