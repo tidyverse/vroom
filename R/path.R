tidyverse/vroom

commit 8be13c10909860ba9f617b8e663beae278abc934

Refinements around multiple files or connections

R/path.R
@@ -7,7 +7,7 @@ reencode_file <- function(path, encoding) {
   if (length(path) > 1) {
     stop(sprintf("Reading files of encoding '%s' can only be done for single files at a time", encoding), call. = FALSE)
   }
-
+  
   if (inherits(path[[1]], "connection")) {
     in_con <- path[[1]]
   } else {
@@ -21,69 +21,91 @@ reencode_file <- function(path, encoding) {
 }
 
 # These functions adapted from https://github.com/tidyverse/readr/blob/192cb1ca5c445e359f153d2259391e6d324fd0a2/R/source.R
-standardise_path <- function(path, user_env = caller_env(2)) {
+standardise_path <- function(path,
+                             arg = caller_arg(path),
+                             call = caller_env(),
+                             user_env = caller_env(2)) {
   if (is.raw(path)) {
     return(list(rawConnection(path, "rb")))
   }
-
+  
   if (inherits(path, "connection")) {
     return(list(standardise_connection(path)))
   }
-
-  if (is_list(path) && all(sapply(path, inherits, "connection"))) {
-    return(lapply(path, standardise_connection))
-  }
-
-  if (is.character(path)) {
-    if (inherits(path, "AsIs")) {
-      if (length(path) > 1) {
-        path <- paste(path, collapse = "\n")
-      }
-      return(list(chr_to_file(path, envir = parent.frame())))
+  
+  if (is_list(path)) {
+    is_connection <- vapply(path, function(x) inherits(x, "connection"), logical(1))
+    if (all(is_connection)) {
+      return(lapply(path, standardise_connection))
     }
-
-    if (any(grepl("\n", path))) {
-      lifecycle::deprecate_soft(
-        "1.5.0",
-        "vroom(file = 'must use `I()` for literal data')",
-        details = c(
-          " " = "",
-          " " = "# Bad:",
-          " " = 'vroom("X,Y\\n1.5,2.3\\n")',
-          " " = "",
-          " " = "# Good:",
-          " " = 'vroom(I("X,Y\\n1.5,2.3\\n"))'
-        ),
-        user_env = user_env
+    if (any(is_connection)) {
+      cli::cli_abort(
+        "{.arg {arg}} cannot be a mix of connection and non-connection inputs",
+        call = call
       )
-      return(list(chr_to_file(path, envir = parent.frame())))
     }
   }
-
+  
+  if (!is.character(path)) {
+    cli::cli_abort(
+      c(
+        "{.arg {arg}} is not one of the supported inputs:",
+        "*" = "A filepath or character vector of filepaths",
+        "*" = "A connection or list of connections",
+        "*" = "Literal or raw input"
+      ),
+      call = call
+    )
+  }
+  
+  if (inherits(path, "AsIs")) {
+    if (length(path) > 1) {
+      path <- paste(path, collapse = "\n")
+    }
+    return(list(chr_to_file(path, envir = parent.frame())))
+  }
+  
+  if (any(grepl("\n", path))) {
+    lifecycle::deprecate_soft(
+      "1.5.0",
+      "vroom(file = 'must use `I()` for literal data')",
+      details = c(
+        " " = "",
+        " " = "# Bad:",
+        " " = 'vroom("X,Y\\n1.5,2.3\\n")',
+        " " = "",
+        " " = "# Good:",
+        " " = 'vroom(I("X,Y\\n1.5,2.3\\n"))'
+      ),
+      user_env = user_env
+    )
+    return(list(chr_to_file(path, envir = parent.frame())))
+  }
+  
   as.list(enc2utf8(path))
 }
 
 standardise_connection <- function(con) {
   # If the connection is `stdin()`, change it to `file("stdin")`, as we don't
   # support text mode connections.
-
+  
   if (con == stdin()) {
     return(file("stdin"))
   }
-
+  
   con
 }
 
 standardise_one_path <- function (path, write = FALSE) {
-
+  
   if (is.raw(path)) {
     return(rawConnection(path, "rb"))
   }
-
+  
   if (!is.character(path)) {
     return(path)
   }
-
+  
   if (is_url(path)) {
     if (requireNamespace("curl", quietly = TRUE)) {
       con <- curl::curl(path)
@@ -94,76 +116,76 @@ standardise_one_path <- function (path, write = FALSE) {
     ext <- tolower(tools::file_ext(path))
     return(
       switch(ext,
-        zip = ,
-        bz2 = ,
-        xz = {
-          close(con)
-          stop("Reading from remote `", ext, "` compressed files is not supported,\n",
-            "  download the files locally first.", call. = FALSE)
-        },
-        gz = gzcon(con),
-        con
-      )
+      zip = ,
+      bz2 = ,
+      xz = {
+        close(con)
+        stop("Reading from remote `", ext, "` compressed files is not supported,\n",
+        "  download the files locally first.", call. = FALSE)
+      },
+      gz = gzcon(con),
+      con
     )
-  }
-
-  path <- enc2utf8(path)
-
-  p <- split_path_ext(basename_utf8(path))
-
-  if (write) {
-    path <- normalizePath_utf8(path, mustWork = FALSE)
-  } else {
-    path <- check_path(path)
-  }
-
-  if (is_installed("archive")) {
-    formats <- archive_formats(p$extension)
-    extension <- p$extension
-    while(is.null(formats) && nzchar(extension)) {
-      extension <- split_path_ext(extension)$extension
-      formats <- archive_formats(extension)
-    }
-    if (!is.null(formats)) {
-      p$extension <- extension
-      if (write) {
-        if (is.null(formats[[1]])) {
-          return(archive::file_write(path, filter = formats[[2]]))
-        }
-        return(archive::archive_write(path, p$path, format = formats[[1]], filter = formats[[2]]))
-      }
-      if (is.null(formats[[1]])) {
-        return(archive::file_read(path, filter = formats[[2]]))
-      }
-      return(archive::archive_read(path, format = formats[[1]], filter = formats[[2]]))
-    }
-  }
-
-  if (!write) {
-    compression <- detect_compression(path)
-  } else {
-    compression <- NA
-  }
-
-  if (is.na(compression)) {
-    compression <- tools::file_ext(path)
-  }
-
-  if (write && compression == "zip") {
-    stop("Can only read from, not write to, .zip", call. = FALSE)
-  }
-
-  switch(compression,
-    gz = gzfile(path, ""),
-    bz2 = bzfile(path, ""),
-    xz = xzfile(path, ""),
-    zip = zipfile(path, ""),
-    if (!has_trailing_newline(path)) {
-      file(path)
-    } else {
-      path
-    }
   )
+}
+
+path <- enc2utf8(path)
+
+p <- split_path_ext(basename_utf8(path))
+
+if (write) {
+  path <- normalizePath_utf8(path, mustWork = FALSE)
+} else {
+  path <- check_path(path)
+}
+
+if (is_installed("archive")) {
+  formats <- archive_formats(p$extension)
+  extension <- p$extension
+  while(is.null(formats) && nzchar(extension)) {
+    extension <- split_path_ext(extension)$extension
+    formats <- archive_formats(extension)
+  }
+  if (!is.null(formats)) {
+    p$extension <- extension
+    if (write) {
+      if (is.null(formats[[1]])) {
+        return(archive::file_write(path, filter = formats[[2]]))
+      }
+      return(archive::archive_write(path, p$path, format = formats[[1]], filter = formats[[2]]))
+    }
+    if (is.null(formats[[1]])) {
+      return(archive::file_read(path, filter = formats[[2]]))
+    }
+    return(archive::archive_read(path, format = formats[[1]], filter = formats[[2]]))
+  }
+}
+
+if (!write) {
+  compression <- detect_compression(path)
+} else {
+  compression <- NA
+}
+
+if (is.na(compression)) {
+  compression <- tools::file_ext(path)
+}
+
+if (write && compression == "zip") {
+  stop("Can only read from, not write to, .zip", call. = FALSE)
+}
+
+switch(compression,
+gz = gzfile(path, ""),
+bz2 = bzfile(path, ""),
+xz = xzfile(path, ""),
+zip = zipfile(path, ""),
+if (!has_trailing_newline(path)) {
+  file(path)
+} else {
+  path
+}
+)
 }
 
 split_path_ext <- function(path) {
@@ -184,48 +206,48 @@ split_path_ext <- function(path) {
 # https://github.com/r-lib/archive/blob/125f9930798dc20fa12cda30319ca3e9a134a409/R/archive.R#L73
 archive_formats <- function(ext) {
   switch(ext,
-    "7z" = list("7zip", "none"),
-
-    "cpio" = list("cpio", "none"),
-
-    "iso" = list("iso9660", "none"),
-
-    "mtree" = list("mtree", "none"),
-
-    "tar" = list("tar", "none"),
-
-    "tgz" = list("tar", "gzip"),
-    "taz" = list("tar", "gzip"),
-    "tar.gz" = list("tar", "gzip"),
-
-    "tbz" = list("tar", "bzip2"),
-    "tbz2" = list("tar", "bzip2"),
-    "tz2" = list("tar", "bzip2"),
-    "tar.bz2" = list("tar", "bzip2"),
-
-    "tlz" = list("tar", "lzma"),
-    "tar.lzma" = list("tar", "lzma"),
-
-    "txz" = list("tar", "xz"),
-    "tar.xz" = list("tar", "xz"),
-
-    "tzo" = list("tar", "lzop"),
-
-    "taZ" = list("tar", "compress"),
-    "tZ" = list("tar", "compress"),
-
-    "tar.zst"= list("tar", "zstd"),
-
-    "warc" = list("warc", "none"),
-
-    "jar" = list("zip", "none"),
-    "zip" = list("zip", "none"),
-
-    "Z" = list(NULL, "compress"),
-
-    "zst" = list(NULL, "zst"),
-
-    NULL)
+  "7z" = list("7zip", "none"),
+  
+  "cpio" = list("cpio", "none"),
+  
+  "iso" = list("iso9660", "none"),
+  
+  "mtree" = list("mtree", "none"),
+  
+  "tar" = list("tar", "none"),
+  
+  "tgz" = list("tar", "gzip"),
+  "taz" = list("tar", "gzip"),
+  "tar.gz" = list("tar", "gzip"),
+  
+  "tbz" = list("tar", "bzip2"),
+  "tbz2" = list("tar", "bzip2"),
+  "tz2" = list("tar", "bzip2"),
+  "tar.bz2" = list("tar", "bzip2"),
+  
+  "tlz" = list("tar", "lzma"),
+  "tar.lzma" = list("tar", "lzma"),
+  
+  "txz" = list("tar", "xz"),
+  "tar.xz" = list("tar", "xz"),
+  
+  "tzo" = list("tar", "lzop"),
+  
+  "taZ" = list("tar", "compress"),
+  "tZ" = list("tar", "compress"),
+  
+  "tar.zst"= list("tar", "zstd"),
+  
+  "warc" = list("warc", "none"),
+  
+  "jar" = list("zip", "none"),
+  "zip" = list("zip", "none"),
+  
+  "Z" = list(NULL, "compress"),
+  
+  "zst" = list(NULL, "zst"),
+  
+  NULL)
 }
 
 is_url <- function(path) {
@@ -236,14 +258,14 @@ check_path <- function(path) {
   if (file.exists(path)) {
     return(normalizePath_utf8(path, mustWork = FALSE))
   }
-
+  
   stop("'", path, "' does not exist",
-    if (!is_absolute_path(path)) {
-      paste0(" in current working directory ('", getwd(), "')")
-    },
-    ".",
-    call. = FALSE
-  )
+  if (!is_absolute_path(path)) {
+    paste0(" in current working directory ('", getwd(), "')")
+  },
+  ".",
+  call. = FALSE
+)
 }
 
 is_absolute_path <- function(path) {
@@ -253,11 +275,11 @@ is_absolute_path <- function(path) {
 zipfile <- function(path, open = "r") {
   files <- utils::unzip(path, list = TRUE)
   file <- files$Name[[1]]
-
+  
   if (nrow(files) > 1) {
     inform(paste0("Multiple files in zip: reading '", file, "'"))
   }
-
+  
   unz(path, file, open = open)
 }
 
@@ -268,9 +290,9 @@ chr_to_file <- function(x, envir = parent.frame()) {
   con <- file(out, "wb")
   writeLines(sub("\n$", "", x), con, useBytes = TRUE)
   close(con)
-
+  
   withr::defer(unlink(out), envir = envir)
-
+  
   normalizePath_utf8(out)
 }
 
@@ -280,49 +302,49 @@ detect_compression <- function(path) {
     return("gz")
   }
   if (length(bytes) >= 6 &&
-    bytes[[1]] == 0xFD &&
-    bytes[[2]] == 0x37 &&
-    bytes[[3]] == 0x7A &&
-    bytes[[4]] == 0x58 &&
-    bytes[[5]] == 0x5A &&
-    bytes[[6]] == 0x00) {
+  bytes[[1]] == 0xFD &&
+  bytes[[2]] == 0x37 &&
+  bytes[[3]] == 0x7A &&
+  bytes[[4]] == 0x58 &&
+  bytes[[5]] == 0x5A &&
+  bytes[[6]] == 0x00) {
     return("xz")
   }
-
+  
   if (length(bytes) >= 3 &&
-    bytes[[1]] == 0x42 &&
-    bytes[[2]] == 0x5a &&
-    bytes[[3]] == 0x68) {
+  bytes[[1]] == 0x42 &&
+  bytes[[2]] == 0x5a &&
+  bytes[[3]] == 0x68) {
     return("bz2")
   }
-
+  
   # normal zip
   if (length(bytes) >= 4 &&
-    bytes[[1]] == 0x50 &&
-    bytes[[2]] == 0x4B &&
-    bytes[[3]] == 0x03 &&
-    bytes[[4]] == 0x04) {
+  bytes[[1]] == 0x50 &&
+  bytes[[2]] == 0x4B &&
+  bytes[[3]] == 0x03 &&
+  bytes[[4]] == 0x04) {
     return("zip")
   }
-
+  
   # empty zip
   if (length(bytes) >= 4 &&
-    bytes[[1]] == 0x50 &&
-    bytes[[2]] == 0x4B &&
-    bytes[[3]] == 0x05 &&
-    bytes[[4]] == 0x06) {
+  bytes[[1]] == 0x50 &&
+  bytes[[2]] == 0x4B &&
+  bytes[[3]] == 0x05 &&
+  bytes[[4]] == 0x06) {
     return("zip")
   }
-
+  
   # spanned zip
   if (length(bytes) >= 4 &&
-    bytes[[1]] == 0x50 &&
-    bytes[[2]] == 0x4B &&
-    bytes[[3]] == 0x07 &&
-    bytes[[4]] == 0x08) {
+  bytes[[1]] == 0x50 &&
+  bytes[[2]] == 0x4B &&
+  bytes[[3]] == 0x07 &&
+  bytes[[4]] == 0x08) {
     return("zip")
   }
-
+  
   NA_character_
 }
 

R/vroom.R
@@ -1,7 +1,8 @@
 #' Read a delimited file into a tibble
 #'
 #' @param file Either a path to a file, a connection, or literal data (either a
-#'   single string or a raw vector).
+#'   single string or a raw vector). `file` can also be a character vector
+#'   containing multiple filepaths or a list containing multiple connections.
 #'
 #'   Files ending in `.gz`, `.bz2`, `.xz`, or `.zip` will be automatically
 #'   uncompressed. Files starting with `http://`, `https://`, `ftp://`, or
@@ -9,9 +10,7 @@
 #'   automatically downloaded and decompressed.
 #'
 #'   Literal data is most useful for examples and tests. To be recognised as
-#'   literal data, the input must be either wrapped with `I()`, be a string
-#'   containing at least one new line, or be a vector containing at least one
-#'   string with a new line.
+#'   literal data, wrap the input with `I()`.
 #' @param delim One or more characters used to delimit fields within a
 #'   file. If `NULL` the delimiter is guessed from the set of `c(",", "\t", " ",
 #'   "|", ":", ";")`.

man/vroom.Rd
@@ -32,7 +32,8 @@ vroom(
 }
 \arguments{
 \item{file}{Either a path to a file, a connection, or literal data (either a
-single string or a raw vector).
+single string or a raw vector). \code{file} can also be a character vector
+containing multiple filepaths or a list containing multiple connections.
 
 Files ending in \code{.gz}, \code{.bz2}, \code{.xz}, or \code{.zip} will be automatically
 uncompressed. Files starting with \verb{http://}, \verb{https://}, \verb{ftp://}, or
@@ -40,9 +41,7 @@ uncompressed. Files starting with \verb{http://}, \verb{https://}, \verb{ftp://}
 automatically downloaded and decompressed.
 
 Literal data is most useful for examples and tests. To be recognised as
-literal data, the input must be either wrapped with \code{I()}, be a string
-containing at least one new line, or be a vector containing at least one
-string with a new line.}
+literal data, wrap the input with \code{I()}.}
 
 \item{delim}{One or more characters used to delimit fields within a
 file. If \code{NULL} the delimiter is guessed from the set of \code{c(",", "\\t", " ", "|", ":", ";")}.}

man/vroom_lines.Rd
@@ -19,7 +19,8 @@ vroom_lines(
 }
 \arguments{
 \item{file}{Either a path to a file, a connection, or literal data (either a
-single string or a raw vector).
+single string or a raw vector). \code{file} can also be a character vector
+containing multiple filepaths or a list containing multiple connections.
 
 Files ending in \code{.gz}, \code{.bz2}, \code{.xz}, or \code{.zip} will be automatically
 uncompressed. Files starting with \verb{http://}, \verb{https://}, \verb{ftp://}, or
@@ -27,9 +28,7 @@ uncompressed. Files starting with \verb{http://}, \verb{https://}, \verb{ftp://}
 automatically downloaded and decompressed.
 
 Literal data is most useful for examples and tests. To be recognised as
-literal data, the input must be either wrapped with \code{I()}, be a string
-containing at least one new line, or be a vector containing at least one
-string with a new line.}
+literal data, wrap the input with \code{I()}.}
 
 \item{n_max}{Maximum number of lines to read.}
 

tests/testthat/test-multi-file.R
@@ -6,49 +6,49 @@ test_that("vroom adds the id column from the filename for one file", {
 test_that("vroom adds the id column from the filename for multiple files", {
   dir <- tempfile()
   dir.create(dir)
-
+  
   splits <- split(mtcars, mtcars$cyl)
   for (i in seq_along(splits)) {
     vroom_write(splits[[i]], file.path(dir, paste0("mtcars_", names(splits)[[i]], ".tsv")), delim = "\t")
   }
-
+  
   files <- list.files(dir, full.names = TRUE)
-
+  
   res <- vroom(files, id = "filename", col_types = list())
-
+  
   # construct what the filename column should look like
   filenames <- paste0("mtcars_", rep(names(splits), vapply(splits, nrow, integer(1))), ".tsv")
-
+  
   expect_equal(basename(res$filename), filenames)
 })
 
 test_that("vroom adds the id column from the filename for multiple connections", {
   dir <- tempfile()
   dir.create(dir)
-
+  
   splits <- split(mtcars, mtcars$cyl)
   for (i in seq_along(splits)) {
     # write_tsv will automatically gzip them
     vroom_write(splits[[i]], file.path(dir, paste0("mtcars_", names(splits)[[i]], ".tsv.gz")), delim = "\t")
   }
-
+  
   files <- list.files(dir, full.names = TRUE)
-
+  
   res <- vroom(files, id = "filename", col_types = list())
-
+  
   # construct what the filename column should look like
   filenames <- paste0("mtcars_", rep(names(splits), vapply(splits, nrow, integer(1))), ".tsv.gz")
-
+  
   expect_equal(basename(res$filename), filenames)
 })
 
 test_that("vroom works with many files", {
   skip_on_os("solaris")
-
+  
   dir <- tempfile()
   dir.create(dir)
   on.exit(unlink(dir, recursive = TRUE))
-
+  
   for (i in seq_len(200)) {
     vroom_write(
       tibble::tibble(
@@ -59,23 +59,27 @@ test_that("vroom works with many files", {
       delim = ","
     )
   }
-
+  
   files <- list.files(dir, pattern = ".*[.]csv", full.names = TRUE)
-
+  
   res <- vroom::vroom(files, col_types = list())
-
+  
   expect_equal(colnames(res), c("x", "y"))
   expect_equal(NROW(res), 2000)
 })
 
 test_that("vroom works with many connections", {
   skip_on_os("solaris")
-
-  dir <- tempfile()
-  dir.create(dir)
-  on.exit(unlink(dir, recursive = TRUE))
-
-  for (i in seq_len(20)) {
+  
+  dir <- withr::local_tempdir()
+  
+  # the number of files is intentionally larger than 128, which has
+  # historically been the maximum number of connections allowed by R
+  # in R >= 4.4, 128 is likely to be the new default, with higher values
+  # allowed
+  # https://github.com/tidyverse/vroom/issues/64
+  # https://github.com/tidyverse/vroom/commit/a41465d70db37ab2bc628ff1e606b71c410fb0e3
+  for (i in seq_len(200)) {
     vroom_write(
       tibble::tibble(
         x = rnorm(10),
@@ -85,12 +89,19 @@ test_that("vroom works with many connections", {
       delim = ","
     )
   }
-
+  
   files <- list.files(dir, pattern = ".*[.]csv[.]gz", full.names = TRUE)
-  connections <- lapply(files, gzfile)
 
+  # vroom manages the connections internally
+  res <- vroom::vroom(files, col_types = list())
+  expect_equal(colnames(res), c("x", "y"))
+  expect_equal(NROW(res), 2000)
+
+  # use explicit connections, so we don't ask for anything close to R's max
+  connections <- lapply(files[1:20], gzfile)
+  
   res <- vroom::vroom(connections, col_types = list())
-
+  
   expect_equal(colnames(res), c("x", "y"))
   expect_equal(NROW(res), 200)
 })
@@ -113,15 +124,15 @@ test_that("vroom works if a file contains no data", {
 
 test_that("vroom works if some files contain no data, regardless of order (#430)", {
   destdir <- withr::local_tempdir("testing-multiple-files")
-
+  
   vroom_write_lines(c("A,B"), file.path(destdir, "header_only.csv"))
   vroom_write_lines(c("A,B"), file.path(destdir, "another_header_only.csv"))
   vroom_write_lines(c("A,B", "1,2"), file.path(destdir, "header_and_one_row.csv"))
-
+  
   files <- file.path(destdir, c("header_only.csv", "header_and_one_row.csv"))
   res <- vroom(files, show_col_types = FALSE)
   expect_equal(res, tibble::tibble(A = 1, B = 2))
-
+  
   files <- file.path(destdir, c(
     "header_only.csv",
     "another_header_only.csv",
@@ -129,7 +140,7 @@ test_that("vroom works if some files contain no data, regardless of order (#430)
   ))
   res <- vroom(files, show_col_types = FALSE)
   expect_equal(res, tibble::tibble(A = 1, B = 2))
-
+  
   files <- file.path(destdir, c(
     "header_only.csv",
     "header_and_one_row.csv",
@@ -137,7 +148,7 @@ test_that("vroom works if some files contain no data, regardless of order (#430)
   ))
   res <- vroom(files, show_col_types = FALSE)
   expect_equal(res, tibble::tibble(A = 1, B = 2))
-
+  
   files <- file.path(destdir, c(
     "header_and_one_row.csv",
     "header_only.csv",
@@ -145,7 +156,7 @@ test_that("vroom works if some files contain no data, regardless of order (#430)
   ))
   res <- vroom(files, show_col_types = FALSE)
   expect_equal(res, tibble::tibble(A = 1, B = 2))
-
+  
   files <- file.path(destdir, c(
     "header_only.csv",
     "another_header_only.csv"

tests/testthat/test-path.R
@@ -178,3 +178,31 @@ test_that("can read fwf file w/ non-ascii characters in path", {
     tibble::tibble(a = c("A", "C"), b = c("B", "D"))
   )
 })
+
+test_that("standardise_path() errors for a mix of connection and not connection", {
+  file <- test_path("multi-file", "foo")
+  conn <- file(test_path("multi-file", "bar"))
+
+  # wrap it, so we can check the caller is displayed correctly
+  f <- function(some_arg_name) {
+    standardise_path(some_arg_name)
+  }
+
+  expect_snapshot(
+    error = TRUE,
+    f(list(file, conn))
+  )
+})
+
+test_that("standardise_path() errors for invalid input", {
+  files <- test_path("multi-file", c("foo", "baz"))
+  # wrap it, so we can check the caller is displayed correctly
+  f <- function(some_arg_name) {
+    standardise_path(some_arg_name)
+  }
+
+  expect_snapshot(
+    error = TRUE,
+    f(as.list(files))
+  )
+})

commit dca0094316a3f16971790450199840fb84662fee

Whitespace
Changes introduced inadvertently in 8be13c10909860ba9f617b8e663beae278abc934

R/path.R
@@ -7,7 +7,7 @@ reencode_file <- function(path, encoding) {
   if (length(path) > 1) {
     stop(sprintf("Reading files of encoding '%s' can only be done for single files at a time", encoding), call. = FALSE)
   }
-  
+
   if (inherits(path[[1]], "connection")) {
     in_con <- path[[1]]
   } else {
@@ -28,11 +28,11 @@ standardise_path <- function(path,
   if (is.raw(path)) {
     return(list(rawConnection(path, "rb")))
   }
-  
+
   if (inherits(path, "connection")) {
     return(list(standardise_connection(path)))
   }
-  
+
   if (is_list(path)) {
     is_connection <- vapply(path, function(x) inherits(x, "connection"), logical(1))
     if (all(is_connection)) {
@@ -45,7 +45,7 @@ standardise_path <- function(path,
       )
     }
   }
-  
+
   if (!is.character(path)) {
     cli::cli_abort(
       c(
@@ -57,14 +57,14 @@ standardise_path <- function(path,
       call = call
     )
   }
-  
+
   if (inherits(path, "AsIs")) {
     if (length(path) > 1) {
       path <- paste(path, collapse = "\n")
     }
     return(list(chr_to_file(path, envir = parent.frame())))
   }
-  
+
   if (any(grepl("\n", path))) {
     lifecycle::deprecate_soft(
       "1.5.0",
@@ -81,31 +81,31 @@ standardise_path <- function(path,
     )
     return(list(chr_to_file(path, envir = parent.frame())))
   }
-  
+
   as.list(enc2utf8(path))
 }
 
 standardise_connection <- function(con) {
   # If the connection is `stdin()`, change it to `file("stdin")`, as we don't
   # support text mode connections.
-  
+
   if (con == stdin()) {
     return(file("stdin"))
   }
-  
+
   con
 }
 
 standardise_one_path <- function (path, write = FALSE) {
-  
+
   if (is.raw(path)) {
     return(rawConnection(path, "rb"))
   }
-  
+
   if (!is.character(path)) {
     return(path)
   }
-  
+
   if (is_url(path)) {
     if (requireNamespace("curl", quietly = TRUE)) {
       con <- curl::curl(path)
@@ -207,46 +207,46 @@ split_path_ext <- function(path) {
 archive_formats <- function(ext) {
   switch(ext,
   "7z" = list("7zip", "none"),
-  
+
   "cpio" = list("cpio", "none"),
-  
+
   "iso" = list("iso9660", "none"),
-  
+
   "mtree" = list("mtree", "none"),
-  
+
   "tar" = list("tar", "none"),
-  
+
   "tgz" = list("tar", "gzip"),
   "taz" = list("tar", "gzip"),
   "tar.gz" = list("tar", "gzip"),
-  
+
   "tbz" = list("tar", "bzip2"),
   "tbz2" = list("tar", "bzip2"),
   "tz2" = list("tar", "bzip2"),
   "tar.bz2" = list("tar", "bzip2"),
-  
+
   "tlz" = list("tar", "lzma"),
   "tar.lzma" = list("tar", "lzma"),
-  
+
   "txz" = list("tar", "xz"),
   "tar.xz" = list("tar", "xz"),
-  
+
   "tzo" = list("tar", "lzop"),
-  
+
   "taZ" = list("tar", "compress"),
   "tZ" = list("tar", "compress"),
-  
+
   "tar.zst"= list("tar", "zstd"),
-  
+
   "warc" = list("warc", "none"),
-  
+
   "jar" = list("zip", "none"),
   "zip" = list("zip", "none"),
-  
+
   "Z" = list(NULL, "compress"),
-  
+
   "zst" = list(NULL, "zst"),
-  
+
   NULL)
 }
 
@@ -258,7 +258,7 @@ check_path <- function(path) {
   if (file.exists(path)) {
     return(normalizePath_utf8(path, mustWork = FALSE))
   }
-  
+
   stop("'", path, "' does not exist",
   if (!is_absolute_path(path)) {
     paste0(" in current working directory ('", getwd(), "')")
@@ -275,11 +275,11 @@ is_absolute_path <- function(path) {
 zipfile <- function(path, open = "r") {
   files <- utils::unzip(path, list = TRUE)
   file <- files$Name[[1]]
-  
+
   if (nrow(files) > 1) {
     inform(paste0("Multiple files in zip: reading '", file, "'"))
   }
-  
+
   unz(path, file, open = open)
 }
 
@@ -290,9 +290,9 @@ chr_to_file <- function(x, envir = parent.frame()) {
   con <- file(out, "wb")
   writeLines(sub("\n$", "", x), con, useBytes = TRUE)
   close(con)
-  
+
   withr::defer(unlink(out), envir = envir)
-  
+
   normalizePath_utf8(out)
 }
 
@@ -310,14 +310,14 @@ detect_compression <- function(path) {
   bytes[[6]] == 0x00) {
     return("xz")
   }
-  
+
   if (length(bytes) >= 3 &&
   bytes[[1]] == 0x42 &&
   bytes[[2]] == 0x5a &&
   bytes[[3]] == 0x68) {
     return("bz2")
   }
-  
+
   # normal zip
   if (length(bytes) >= 4 &&
   bytes[[1]] == 0x50 &&
@@ -326,7 +326,7 @@ detect_compression <- function(path) {
   bytes[[4]] == 0x04) {
     return("zip")
   }
-  
+
   # empty zip
   if (length(bytes) >= 4 &&
   bytes[[1]] == 0x50 &&
@@ -335,7 +335,7 @@ detect_compression <- function(path) {
   bytes[[4]] == 0x06) {
     return("zip")
   }
-  
+
   # spanned zip
   if (length(bytes) >= 4 &&
   bytes[[1]] == 0x50 &&
@@ -344,7 +344,7 @@ detect_compression <- function(path) {
   bytes[[4]] == 0x08) {
     return("zip")
   }
-  
+
   NA_character_
 }
 