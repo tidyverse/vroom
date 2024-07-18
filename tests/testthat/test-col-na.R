255cabd029ac246b80dffbfae76d62aff569e3ba
Kyle Husmann <kyle.husmann@gmail.com>
2024-07-18 10:42:20 -0700

    add test to exercise the col_guess()->resolve_collectors() code path

--- tests/testthat/test-col-na.R
+++ tests/testthat/test-col-na.R
@@ -14,3 +14,20 @@ test_that("collector-level na overrides global na", {
     )
   )
 })
+
+test_that("collector-level na works with col_guess", {
+  test_vroom(
+    "a,b,c\n1,1.1,REFUSED\n2,REFUSED,NA\nOMITTED,2.2,OMITTED\n",
+    col_types = cols(
+      a = col_guess(na = "OMITTED"),
+      b = col_guess(na = "REFUSED"),
+      c = col_guess(),
+    ),
+    na = "NA",
+    equals = tibble::tibble(
+      a = c(1, 2, NA),
+      b = c(1.1, NA, 2.2),
+      c = c("REFUSED", NA, "OMITTED"),
+    )
+  )
+})
